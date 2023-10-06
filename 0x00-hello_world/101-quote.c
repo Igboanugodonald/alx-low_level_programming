a6fe28a75bb5cf2d2fa31ae3a4d958b140989f7c
Igboanugodonald <digboanugo7@gmail.com>
2023-10-06 11:57:15 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entry point
- * echo - prints the string
  *
- * Return: 1 (as requested)
+ * Return: 1
  */
 int main(void)
 {
-	echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
