b2c4aa520f4ba36df66f5798b0ef8ee1b655f996
Igboanugodonald <digboanugo7@gmail.com>
2023-10-17 05:46:21 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,18 +6,18 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char letter = 'a';
+{	
+	 char letter = 'a';
 
-	while (letter <= 'z')
-	{
-	if	(letter != 'e' && letter != 'q')
-	{
-		putchar(letter);
-	}
-		letter++;
-	}
+    while (letter <= 'z')
+    {
+        if (letter != 'e' && letter != 'q')
+        {
+            putchar(letter);
+        }
+        letter++;
+    }
 
-		putchar('\n');
-		return (0);
+    putchar('\n');
+    return (0);
 }
