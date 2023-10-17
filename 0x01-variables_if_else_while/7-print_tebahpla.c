820883ba5da6d1b7ae787f0b3bd7fa3bd15d0558
Igboanugodonald <digboanugo7@gmail.com>
2023-10-17 12:44:13 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char _dee = 'z';
+
+	while (_dee >= 'a')
+	{
+		putchar(_dee);
+		_dee--;
+	}
+		putchar('\n');
+		return (0);
+}
