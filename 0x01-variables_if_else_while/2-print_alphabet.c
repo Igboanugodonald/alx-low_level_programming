9090a0fe6703b76e1b1b16e3590c29db9c50241a
Igboanugodonald <digboanugo7@gmail.com>
2023-10-17 03:31:22 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+		char _alphabet='a';
+		while(_alphabet <= 'z')
+		{
+			putchar(_alphabet);
+			_alphabet++;
+		}
+		putchar('\n');
+		return (0);
+}
