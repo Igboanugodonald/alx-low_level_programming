c4bf7d9f96780b75c3d1c80a8e92b51ef7ce6c94
Igboanugodonald <digboanugo7@gmail.com>
2023-10-17 07:22:09 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int i = 1;
+
+		for (i = 1; i < 10; i++)
+		{
+			printf("%d\n", i);
+		}
+			i++;
+			return (0);
+}
