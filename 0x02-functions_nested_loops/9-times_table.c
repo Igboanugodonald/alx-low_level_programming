dc64d58566ad1637c9ac79c658c0b0c8055ec0d7
Igboanugodonald <digboanugo7@gmail.com>
2023-10-24 08:19:09 -0700

    9 times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,19 +8,19 @@ void times_table(void)
 {
 	 int i, j, result;
 
-	for (i = 0; i <= 9; i++) 
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++) 
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
 		if (j == 0)
 		{
-			printf("%d", result);
+			printf("%2d", result);
 		}
 		else
 		{
-		printf(", %d", result);
-		}	
+		printf(", %2d", result);
+		}
 	}
 	printf("\n");
 	}
