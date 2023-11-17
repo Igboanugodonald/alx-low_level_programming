9a1ef23a9ed4c0be5df36e7d648658b8fbd599a3
Igboanugodonald <digboanugo7@gmail.com>
2023-11-16 19:15:28 -0800

    header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,7 +15,7 @@ typedef struct list_s
  char *str;
  unsigned int len;
  struct list_s *next;
-}
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
